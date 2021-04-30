37d69d7a561bbc7041f8a7949281e8c0f7e7c656
Pedro Ramaciotti <vostokru@gmail.com>
2021-04-30 07:17:32 +0200

    benchwork working

--- benchwork.py
+++ benchwork.py
@@ -1,40 +1,56 @@
 import pandas as pd
 import numpy as np
+import os
 
-with open('requirements.txt') as f:
-    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
-# # Assigning ids to users in the log
-# Log_df['user_node_id'] = Log_df['user_id'].map(pd.Series(index=LogUsers_df['user_id'].values,data=LogUsers_df['user_node_id'].values))
-# Log_df['user_node_id'].isna().sum() # -> 0
-# # we subtract elements 
-# citation_count=Log_df[['user_node_id',top_level_object]].groupby(top_level_object).count()['user_node_id']
-# citation_count.size # -> 12.620.446
-# (citation_count>1).sum() # -> 1.927.854
-# # computing bipartite graph in community structure format
-# bipartite_df = Log_df.loc[Log_df[top_level_object].isin(citation_count[citation_count>1].index.values),['user_node_id',top_level_object]]
-# bipartite_df.duplicated().sum() # -> 0
-# bipartite_df.shape[0] # -> 8.486.251
-# bipartite_df.normalized_url.nunique() # -> 1.927.854
-# bipartite_df.user_node_id.nunique() # -> 34.872
-# bipartite_df.shape[0]/(bipartite_df.normalized_url.nunique()*bipartite_df.user_node_id.nunique())
-# # -> density = 0.000126
-# bipartite=bipartite_df.groupby(top_level_object)['user_node_id'].apply(lambda n: ' '.join(np.sort(n))).reset_index(drop=True)
-# bipartite.shape[0] # -> 1.927.854 AOK
-# # Checking that there are no repeated bottom nodes per top nodes
-# bipartite.apply(lambda tn: len(tn.split(' '))!=len(set(tn.split(' '))) ).sum() # -> 0, AOK!
-# # Deleting URLs cited by the same set of users (in whatever commutation)
-# bipartite.duplicated().sum() # -> 340.422 urls/domains are cited by the same set of users
-# bipartite=bipartite[~bipartite.duplicated()] # we have to eliminate them
-# bipartite.shape[0] # 1.587.432
-# # saving to file
-# bipartite.to_csv('user_citation_bipartite.csv',header=False,index=False)
-# # computing weighted projected graph
-# os.system('./comcore1 1 user_citation_bipartite.csv user_citation_projection.csv')
-# # reading weighted projected graph file
-# projected=pd.read_csv('user_citation_projection.csv',header=None,names=['source','target','weight'],
-#                       sep=' ',dtype={'source':str,'target':str,'weight':int})
-# pd.concat([projected['source'],projected['target']],axis=0).nunique() # -> 34.872 AOK!
-# projected.shape[0] # -> 78.365.661 edges
-# projected[['source','target']].duplicated().sum() # -> 0 AOK!
-# # Exporting a graph with weight threshold for qualitative inspections
-# projected.loc[projected['weight']>=20,['source','target']].to_csv('gephi_user_projected_graph.csv',sep=';',header=False,index=False)
+#############################
+# In puts of the function
+#############################
+
+projection = 'A_id' # the part of the bipartite whose nodes will be retained in the projection
+
+# Loading a biparte network as a list of edges.
+df = pd.read_csv('sample_graphs/sample_bipartite_1.csv')
+
+#############################
+# The function
+#############################
+
+# Selecting the projection
+# Here there are 3 options: either a proper columns has been selected, 
+# either an unexisting column has been selected,
+# either no column has been.
+if projection is None:
+    projection=df.columns[0]
+elif projection not in df.columns:
+    raise ValueError("Selected columns not in graph")
+
+# Lookup table between projected nodes and integer numbering
+lut = pd.DataFrame.from_dict({projection:df[projection].unique(),'int_id':np.arange(1,df[projection].nunique()+1,dtype=int)})
+
+# The nodes of part to be projected need integer numbering, starting from 1
+df['int_id'] = df[projection].map(pd.Series(index=lut[projection].values,data=lut['int_id'].values))
+    
+# Writing the bipartite graph in community structure
+# Each node of the non-projected side is a "community"
+# We write one line per community, each line contains the nodes from the projected side that belong (are connected) 
+# to the community
+df['int_id']=df['int_id'].astype(str)
+non_projected_part = [c for c in df.columns[:-1] if c not in [projection,'int_id']][0]
+communitized_df = df[['int_id',non_projected_part]].groupby(non_projected_part)['int_id'].apply(lambda n: ' '.join(np.sort(n))).reset_index(drop=True)
+
+# saving bipartite in community structure format to file
+communitized_df.to_csv('input.csv',header=False,index=False)
+# computing weighted projected graph
+os.system('./pandasgraph/comcore1 1 input.csv output.csv')
+# reading weighted projected graph file
+projected=pd.read_csv('output.csv',header=None,names=['source','target','weight'],
+                      sep=' ',dtype={'source':int,'target':int,'weight':int})
+projected['source']=projected['source'].map(pd.Series(index=lut['int_id'].values,data=lut[projection].values))
+projected['target']=projected['target'].map(pd.Series(index=lut['int_id'].values,data=lut[projection].values))
+
+
+
+
+
+
+
